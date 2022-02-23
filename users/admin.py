0dfa234d48fae84d133850456e479bd14a14ee51
ByeonSeungHa <a01051205048@gmail.com>
2022-02-23 12:50:58 +0900

    2022-02-23

---
+++ users/admin.py
@@ -0,0 +1,29 @@
+from locale import currency
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+
+# Register your models here.
